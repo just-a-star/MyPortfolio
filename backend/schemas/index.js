7a23983cd0efaba80e307369997cc065278bfdd1
--global <21523283@students.uii.ac.id>
2024-01-02 00:01:30 +0700

    fix

---
+++ backend/schemas/index.js
@@ -0,0 +1,19 @@
+import about from './abouts'
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import contact from './contact'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+  about,
+  works,
+  testimonials,
+  brands,
+  experiences,
+  skills,
+  contact,
+  workExperience,
+]
